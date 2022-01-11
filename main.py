f59e2167224d106a3441a1a7b00999656a271bbb
rwhitesensei <86308567+rwhitesensei@users.noreply.github.com>
2022-01-11 13:53:53 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+
+# importing flask module fro
+from flask import Flask, render_template,request
+
+# initializing a variable of Flask
+app = Flask(__name__)
+
+
+# decorating index function with the app.route with url as /login
+@app.route('/login')
+def index():
+   return render_template('login.html')
+
+
+@app.route('/FlaskTutorial',  methods=['POST'])
+def success():
+   if request.method == 'POST':
+       email = request.form['email']
+       return render_template('success.html', email=email)
+   else:
+       pass
+if __name__ == "__main__":
+   app.run()
+
+
+
+
